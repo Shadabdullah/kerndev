82d469cb7fe40abc619a62c8395789d872e5d05c
shadabdullah <shadabdullah101@gmail.com>
2024-09-09 14:24:25 +0530

    project init

--- vicharak/userspace/filler.c
+++ vicharak/userspace/filler.c
@@ -1,22 +1,40 @@
+#include <stdio.h>
 #include <fcntl.h>
-#include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 #include <sys/ioctl.h>
+#include <string.h>
+#include <stdlib.h>
+
 #define DRIVER_NAME "/dev/shad"
-#define PUSH_DATA _IOW('a', 'b', struct data * )
+#define PUSH_DATA _IOW('a', 'b', struct data *)
+
 struct data {
-int length;
-char * data;
-};int main(void) {
-int fd = open(DRIVER_NAME, O_RDWR);
-struct data * d = malloc(sizeof(struct data));
-d->length = 3;
-d->data = malloc(3);
-memcpy(d->data, "xyz", 3);
-int ret = ioctl(fd, PUSH_DATA, d);
-close(fd);
-free(d->data);
-free(d);
-return ret;
+    int length;
+    char *data;
+};
+
+int main(void) {
+    int fd = open(DRIVER_NAME, O_RDWR);
+    if (fd < 0) {
+        perror("Failed to open the device");
+        return -1;
+    }
+
+    struct data *d = malloc(sizeof(struct data));
+    d->length = 3;
+    d->data = malloc(3);
+    memcpy(d->data, "xyz", 3);
+
+    int ret = ioctl(fd, PUSH_DATA, d);
+    if (ret < 0) {
+        perror("couldn't push the data into queue");
+    }else{
+
+  printf(" %s data pushed successfully" ,d->data);
+  }
+
+    close(fd);
+    free(d->data);
+    free(d);
+    return ret;
 }
--- vicharak/userspace/reader.c
+++ vicharak/userspace/reader.c
@@ -1,23 +1,52 @@
 #include <stdio.h>
 #include <fcntl.h>
-#include <stdlib.h>
 #include <unistd.h>
 #include <sys/ioctl.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
 #define DRIVER_NAME "/dev/shad"
-#define POP_DATA _IOR('a', 'c', struct data * )
+#define POP_DATA _IOR('a', 'c', struct data *)
+
 struct data {
-int length;
-char * data;
+    int length;
+    char *data;
 };
+
 int main(void) {
-int fd = open(DRIVER_NAME, O_RDWR);
-struct data * d = malloc(sizeof(struct data));
-d->length = 3;
-d->data = malloc(3);
-int ret = ioctl(fd, POP_DATA, d);
-printf("%s\n", d->data);
-close(fd);
-free(d->data);
-free(d);
-return ret;
+    int fd = open(DRIVER_NAME, O_RDWR);
+    if (fd < 0) {
+        perror("Failed to open the device");
+        return -1;
+    }
+
+    struct data *d = malloc(sizeof(struct data));
+    if (d == NULL) {
+        perror("Failed to allocate memory for struct data");
+        close(fd);
+        return -1;
+    }
+
+    d->length = 2;  // We want to pop 3 bytes
+    d->data = malloc(d->length + 1);  // +1 for null terminator
+    if (d->data == NULL) {
+        perror("Failed to allocate memory for data buffer");
+        free(d);
+        close(fd);
+        return -1;
+    }
+
+    int ret = ioctl(fd, POP_DATA, d);
+    if (ret < 0) {
+            perror("Failed to pop data");
+    } else {
+        d->data[d->length] = '\0';  // Null-terminate the string
+        printf("Popped data: %s\n", d->data);
+    }
+
+    close(fd);
+    free(d->data);
+    free(d);
+    return ret < 0 ? -1 : 0;
 }
